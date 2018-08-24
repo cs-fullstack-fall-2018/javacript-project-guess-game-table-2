77d50f3aa1c39d540a7c2f0ddfcda2db8ba0c568
Jordan Parker <parkerj4321@gmail.com>
2018-08-24 14:42:45 -0500

    Something

--- showhide.js
+++ showhide.js
@@ -1,18 +1,33 @@
 var clicks = 0;
 var targets = 0;
 var hits = 0;
+var targetKount = 1;
+var theGo = document.getElementById("goGetIt");
+const targetTime = 2000;
+var timeOut;
 
 function letsRock() {
-    var theGo = document.getElementById("goGetIt");
+
     theGo.onclick = function () {
-        // Get random number of targets and do setup
-        var targetKount = document.getElementById("numberOfTargets").value;
-        var targetTime = document.getElementById("displayTime").value;
         // No start the game!
         setUpTargetsAndPlay(targetKount, targetTime);
+        theGo.disabled = true;
+        timesUp();
+
+
     };
+ }
+
+function timesUp() {
+    timeOut = setTimeout(function () {
+        theGo.disabled = false;
+        alert('Time is Up')
+    },5000);
 }
 
+function clearTime() {
+    clearTimeout(timeOut);
+}
 // Utility function to get a random table cell number
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -27,25 +42,37 @@ function clickedTarget() {
 
 // The main function that sets up targets and starts a game
 function setUpTargetsAndPlay(numberOfTargets, displayTimeMs) {
+    var theGo = document.getElementById("goGetIt");
     clicks = 0;
     targets = numberOfTargets;
     hits = 0;
-
+    cleanup();
     // Setup click detection for the entire table
     $("table").on("click", function () {
 
         clicks += 1;
         // alert("clicked. Max = " + clicks);
+
         if (clicks == targets) {
             alert("No more clicks! You got " + hits + " out of " + targets);
             // Turn off click detection
-            $("td").off("click");
-            $("table").off("click");
-            $(".targetImg").remove();
+            // $("td").off("click");
+            // $("table").off("click");
+            // $(".targetImg").remove();
+            // theGo.disabled = false;
+            // clearTime();
+            cleanup();
         }
+        if(hits == targets) {
+            targetKount += 1;
+            alert("You progress to the next level");
+        }
+
+
+
     });
 
-    console.log("Selecting " + targets + " targets")
+    console.log("Selecting " + targets + " targets");
     // Get the number of targets specified and randomly picks cells to display them in for the target table
     for (var x = 0; x < targets; x++) {
         var targetNum = getRandomInt(1, 50); // Pick a random table cell
@@ -53,9 +80,19 @@ function setUpTargetsAndPlay(numberOfTargets, displayTimeMs) {
         var tdID = "td" + targetNum;
         var imgID = "img" + targetNum;
 
-        $('#' + tdID).on("click", clickedTarget).append("<img id = " + imgID + " class= 'targetImg' src='nemo1.png'>");
-        $('#' + imgID).delay(2000).show(0);
+        $('#' + tdID).on("click", clickedTarget).append("<img id = " + imgID + " class= 'targetImg' src='nemo1.png' width= '75' height = '75'>");
         $('#' + imgID).delay(displayTimeMs).hide(0);
     }
 
+
+
 };
+
+function cleanup() {
+    $("td").off("click");
+    $("table").off("click");
+    $(".targetImg").remove();
+    theGo.disabled = false;
+    clearTime();
+}
+
